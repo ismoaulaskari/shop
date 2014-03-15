97a15bf0dd0854f9578fa76af99e24e736bd40b5
ismoaulaskari <ismo@aulaskari.com>
2014-03-15 18:03:51 +0200

    generoitu rails 2.3-projektipohja

---
+++ shop/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '744ea0c3d329694d14bb48a53952f51d02690158a1bd249f4fefd01a6cf015ea7ec7b2f629edaff16f007c14fdd595f4f5f1a4e7eff105ff4e982374a3eda2b8';
