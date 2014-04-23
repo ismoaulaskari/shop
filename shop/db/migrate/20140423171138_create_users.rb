d737ff563b5ad23e636f7a53cbffa667b5439e0e
aulaskar <aulaskar@517ebd9f-d2a8-4732-a760-e58524da3951>
2014-04-23 17:37:12 +0000

    users scaffold
    git-svn-id: https://iste2.it.helsinki.fi/svn/shop/trunk@9 517ebd9f-d2a8-4732-a760-e58524da3951

---
+++ shop/db/migrate/20140423171138_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
