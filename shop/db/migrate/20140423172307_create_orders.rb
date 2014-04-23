4c8b38fbe9051ee51732fba749b850681ce82dbb
ismoaulaskari <ismo@aulaskari.com>
2014-04-23 20:23:38 +0300

    orders scaffold

---
+++ shop/db/migrate/20140423172307_create_orders.rb
@@ -0,0 +1,22 @@
+class CreateOrders < ActiveRecord::Migration
+  def self.up
+    create_table :orders do |t|
+      t.integer :sum
+      t.string :paymethod
+      t.string :name
+      t.string :address
+      t.string :zip
+      t.string :city
+      t.string :country
+      t.string :phone
+      t.string :email
+      t.datetime :modified
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :orders
+  end
+end
