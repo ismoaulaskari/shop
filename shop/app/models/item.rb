fe5fb5f25d2691e5c6cb7f2d7d993af3a90006c4
ismoaulaskari <ismo@aulaskari.com>
2014-04-30 11:19:31 +0300

    constraintteja ja validointeja paikoilleen

--- shop/app/models/item.rb
+++ shop/app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ActiveRecord::Base
   belongs_to :category
+  validates_presence_of :name, :description, :price, :saldo
 end
--- shop/app/models/user.rb
+++ shop/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :orders
   has_many :items, through :orders
+  has_many :news
 end
